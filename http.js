a40e178447cedfe71afdd9e3bd25fb7388f0211d
elbekbek <amirkhiat19@gmail.com>
2025-02-07 00:36:11 +0100

    allowed methods

--- http.js
+++ http.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 async function check_website(url){
     try { 
+        //cheking reachability
         const resp = await axios.head(url);
         if (resp.status == 200) {
             console.log('server is alive');
@@ -10,11 +11,22 @@ async function check_website(url){
             console.warn("server returned status code :"+ resp.status);
         }
 
+        //fetching server details
 
+        const getresp = await axios.get(url);
+        const srvheader = getresp.headers['server'];// i used ['server'] to be more specific 
+        if (srvheader){
+            console.log('server header :'+ srvheader);
+        }else{
+            console.warn('server header not found');
+        }
+
+        //checking allowed methods
+        const optionsresp = await axios.options(url);
 
 
         }catch (error) {
-            console.error("error while checking website :"+ error.message);
+            console.error("Error: Unable to reach server :"+ error.message);
         }
 
 
